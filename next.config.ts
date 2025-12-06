7e73e21a463dfd59c7e5e4b6c5ba0fc2848d9309
Thomas Pinda <thomaspinda23@gmail.com>
2025-12-06 19:21:08 -0300

    Arreglo

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-  forceSwcTransforms: true, // A veces ayuda con Next.js 13+
-  },
+
 };
 
 export default nextConfig;
